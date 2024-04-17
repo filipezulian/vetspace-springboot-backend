0de54685ea6ed484916f08690567bb3e35a30b07
Gabriela Westphal <gabiwestphal@hotmail.com>
2024-04-16 23:43:34 -0300

    Inicialização Springboot
    add das pastas padrões necessárias para rodar spring

---
+++ vetspace/src/main/java/com/pin/vetspace/VetspaceApplication.java
@@ -0,0 +1,13 @@
+package com.pin.vetspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetspaceApplication.class, args);
+	}
+
+}
---
+++ vetspace/src/test/java/com/pin/vetspace/VetspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pin.vetspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
